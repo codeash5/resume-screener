51af0f840f738f32e73e0db8f3cd92c381b2749f
codeash5 <ashwinibhawalkar256@gmail.com>
2025-08-10 23:31:19 +0530

    Fix file path to sample resume to ..//sample_resumes/sample1.docx

--- src/resume_screener.py
+++ src/resume_screener.py
@@ -7,8 +7,8 @@ def read_resume(file_path):
         full_text.append(para.text)
     return '\n'.join(full_text)
 
-if __name__ == "_main_":
-    sample_resume = 'sample_resumes/sample1.docx'  # adjust path if needed
+if __name__ == "__main__":
+    sample_resume = '../sample_resumes/sample1.docx'  # adjust path if needed
     text = read_resume(sample_resume)
     if text:
         print("Resume Text:\n")
